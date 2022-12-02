500b054c4656d76234234d384610989f209aad89
JorgeAraya99 <jorge.araya.v@ug.uchile.cl>
2022-12-01 21:19:52 -0300

    avances 0112

--- Tarea_2/Server/DatabaseWork.py
+++ Tarea_2/Server/DatabaseWork.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 
 
 #Funciones de guardado de data para cada protocolo
-def DataSave_1(header, dict, db_file):
+def DataSave_1(header, dict, db_file, conf):
     conn = None
 
     try:
@@ -11,9 +11,11 @@ def DataSave_1(header, dict, db_file):
     except Error as e:
         print(e)
     
-    conn.execute(''' INSERT INTO Datos_1 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Log (Id_device, Status_report, Battery_level, Time_client, Conf_peripheral) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], dict["Batt_level"], dict["Time"]))
 
-    conn.execute(''' INSERT INTO Datos_2 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Datos_1 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
+
+    conn.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
 
     conn.close()
 
@@ -25,9 +27,11 @@ def DataSave_2(header, dict, db_file):
     except Error as e:
         print(e)
     
-    conn.execute(''' INSERT INTO Datos_1 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Log (Id_device, Status_report, Battery_level, Time_client) VALUES (?, ?, ?) ''', (header["ID_device"], header["Status"], dict["Batt_level"], dict["Time"]))
 
-    conn.execute(''' INSERT INTO Datos_2 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO) VALUES (?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"]))
+
+    conn.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
 
     conn.close()
 
@@ -39,9 +43,11 @@ def DataSave_3(header, dict, db_file):
     except Error as e:
         print(e)
     
-    conn.execute(''' INSERT INTO Datos_1 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Log (Id_device, Status_report, Battery_level, Time_client) VALUES (?, ?, ?) ''', (header["ID_device"], header["Status"], dict["Batt_level"], dict["Time"]))
 
-    conn.execute(''' INSERT INTO Datos_2 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO, RMS) VALUES (?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"], dict["RMS"]))
+
+    conn.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
 
     conn.close()
 
@@ -53,9 +59,11 @@ def DataSave_4(header, dict, db_file):
     except Error as e:
         print(e)
     
-    conn.execute(''' INSERT INTO Datos_1 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Log (Id_device, Status_report, Battery_level, Time_client) VALUES (?, ?, ?) ''', (header["ID_device"], header["Status"], dict["Batt_level"], dict["Time"]))
 
-    conn.execute(''' INSERT INTO Datos_2 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO, RMS, Amp_x, Freq_x, Amp_y, Freq_y, Amp_z, Freq_z) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"], dict["RMS"], dict["Ampx"] , dict["Freqx"] , dict["Ampy"] , dict["Freqy"] , dict["Ampz"] , dict["Freqz"]))
+
+    conn.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
 
     conn.close()
 
@@ -67,30 +75,34 @@ def DataSave_5(header, dict, db_file):
     except Error as e:
         print(e)
     
-    conn.execute(''' INSERT INTO Datos_1 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Log (Id_device, Status_report, Battery_level, Time_client) VALUES (?, ?, ?) ''', (header["ID_device"], header["Status"], dict["Batt_level"], dict["Time"]))
 
-    conn.execute(''' INSERT INTO Datos_2 () VALUES () ''', (header[1], dict[1]))
+    conn.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO) VALUES (?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"]))
+
+    conn.execute(''' INSERT INTO Datos_2 (Id_device, Time_client, Racc_x, Racc_y, Racc_z, Rgyr_x, Rgyr_y, Rgyr_z) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Raccx"], dict["Raccy"], dict["Raccy"], dict["Rgyrx"], dict["Rgyry"], dict["Rgyrz"]))
 
     conn.close()
 
 
 #funcion que guarda en data segun protocolo
-def DataSave(header, dict, protocol, db_file):
+def DataSave(header, dict, protocol, db_file, conf):
     match protocol:
         case 1:
-            DataSave_1(header, dict, db_file)
+            DataSave_1(header, dict, db_file, conf)
         case 2:
-            DataSave_2(header, dict, db_file)
+            DataSave_2(header, dict, db_file, conf)
         case 3:
-            DataSave_3(header, dict, db_file)
+            DataSave_3(header, dict, db_file, conf)
         case 4:
-            DataSave_4(header, dict, db_file)
+            DataSave_4(header, dict, db_file, conf)
         case 5:
-            DataSave_5(header, dict, db_file)
+            DataSave_5(header, dict, db_file, conf)
         case _:
             print("protocolo invalido")
 
 #funcion que guarda en Log
+
+"""
 def LogSave(header, dict, db_file):
     conn = None
 
@@ -102,7 +114,7 @@ def LogSave(header, dict, db_file):
     conn.execute(''' INSERT INTO Log () VALUES () ''', (header[1], dict[1]))
 
     conn.close()
-
+"""
 #funcion que haga query de configuracion
 def getConfig():
     pass
--- Tarea_2/Server/mainServer.py
+++ Tarea_2/Server/mainServer.py
@@ -11,3 +11,26 @@ from DatabaseWork import *
 
 
 #Manadr config inicial a la ESP
+
+
+def mainSever(x):
+    #revisar CI
+
+    #Guardar en DB
+
+    #Prender server correspondiente
+
+    match x:
+        case "BLED":
+            pass
+        case "BLEC":
+            pass
+        case "TCPD":
+            pass
+        case "TCPC":
+            pass
+        case "UCP":
+            pass
+        case _:
+            pass
+    
