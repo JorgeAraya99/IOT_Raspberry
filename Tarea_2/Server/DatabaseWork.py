1cc3749612bab51511068fe8840ec7d675069fd3
JorgeAraya99 <jorge.araya.v@ug.uchile.cl>
2022-12-03 23:04:28 -0300

    arreglo de inserts

--- Tarea_2/Server/DatabaseWork.py
+++ Tarea_2/Server/DatabaseWork.py
@@ -15,9 +15,15 @@ def DataSave_1(header, dict, db_file, conf):
 
     cur.execute(''' INSERT INTO Log (Id_device, Status_report, Protocol_report, Battery_level, Conf_peripheral, Time_client, configuration_Id_device) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], header["Protocol"], dict["Batt_level"], conf,dict["Time"], header["ID_device"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_1 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
+
+    conn.commit()
 
     conn.close()
 
@@ -33,9 +39,15 @@ def DataSave_2(header, dict, db_file, conf):
 
     cur.execute(''' INSERT INTO Log (Id_device, Status_report, Protocol_report, Battery_level, Conf_peripheral, Time_client, configuration_Id_device) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], header["Protocol"], dict["Batt_level"], conf,dict["Time"], header["ID_device"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO) VALUES (?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
+
+    conn.commit()
 
     conn.close()
 
@@ -51,9 +63,15 @@ def DataSave_3(header, dict, db_file, conf):
 
     cur.execute(''' INSERT INTO Log (Id_device, Status_report, Protocol_report, Battery_level, Conf_peripheral, Time_client, configuration_Id_device) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], header["Protocol"], dict["Batt_level"], conf,dict["Time"], header["ID_device"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO, RMS) VALUES (?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"], dict["RMS"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
+
+    conn.commit()
 
     conn.close()
 
@@ -69,9 +87,15 @@ def DataSave_4(header, dict, db_file, conf):
 
     cur.execute(''' INSERT INTO Log (Id_device, Status_report, Protocol_report, Battery_level, Conf_peripheral, Time_client, configuration_Id_device) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], header["Protocol"], dict["Batt_level"], conf,dict["Time"], header["ID_device"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO, RMS, Amp_x, Freq_x, Amp_y, Freq_y, Amp_z, Freq_z) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"], dict["RMS"], dict["Amp_x"] , dict["Frec_x"] , dict["Amp_y"] , dict["Frec_y"] , dict["Amp_z"] , dict["Frec_z"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_2 (Id_device, Time_client) VALUES (?, ?) ''', (header["Id_device"], dict["Time"]))
+
+    conn.commit()
 
     conn.close()
 
@@ -87,9 +111,15 @@ def DataSave_5(header, dict, db_file, conf):
 
     cur.execute(''' INSERT INTO Log (Id_device, Status_report, Protocol_report, Battery_level, Conf_peripheral, Time_client, configuration_Id_device) VALUES (?, ?, ?, ?, ?) ''', (header["ID_device"], header["Status"], header["Protocol"], dict["Batt_level"], conf,dict["Time"], header["ID_device"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_1 (Id_device, Time_client, Temperature, Press, Hum, CO) VALUES (?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["Temp"], dict["Press"], dict["Hum"], dict["CO"]))
 
+    conn.commit()
+
     cur.execute(''' INSERT INTO Datos_2 (Id_device, Time_client, Racc_x, Racc_y, Racc_z, Rgyr_x, Rgyr_y, Rgyr_z) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ''', (header["Id_device"], dict["Time"], dict["RAcc_x"], dict["RAcc_y"], dict["RAcc_z"], dict["Rgyr_x"], dict["Rgyr_y"], dict["Rgyr_z"]))
+
+    conn.commit()
 
     conn.close()
 
@@ -121,7 +151,7 @@ def getConfig(ID, db_file):
 
     cur = conn.cursor()
 
-    order = "SELECT * FROM Configuration WHERE Id_device = " + str(ID) 
+    order = "SELECT * FROM Configuration WHERE Id_device = " + str(ID) + ";"
 
     cur.execute(order)
 
